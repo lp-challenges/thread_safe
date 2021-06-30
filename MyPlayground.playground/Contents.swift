376d905e69f6937d787a215fb31df7349c634ed2
Lucas <lucaspedrazoli@gmail.com>
2021-06-30 14:07:52 -0300

    thread safe hash

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,16 +4,33 @@ var greeting = "Hello, playground"
 
 class Logger {
 
-  private var hash: [String: String] = [:]
+  private var hash: [String: Int] = [:]
   private let queue = DispatchQueue(label: "my_queue", attributes: .concurrent)
 
-  func read(_ key: String) -> String? {
-    return self.hash[key]
+  func read(_ key: String) -> Int? {
+    var value: Int?
+    queue.sync {
+      value = hash[key]
+    }
+    return value
   }
 
-  func write(key: String, value: String) {
-    queue.asyncAfter(deadline: DispatchTime.now(), qos: .default, flags: .barrier, execute: {
+  func write(key: String, value: Int) {
+    queue.async(qos: .default, flags: .barrier) {
       self.hash[key] = value
-    })
+    }
   }
 }
+
+func run() {
+  let logger = Logger()
+  var number: Int = 0
+  DispatchQueue.concurrentPerform(iterations: 50) { index in
+    logger.write(key: "key", value: number)
+    number += 1
+    print(logger.read("key") ?? "error")
+  }
+}
+
+run()
+
