780e2f806356e2ffa3a554b9c80483df647844c5
Lucas <lucaspedrazoli@gmail.com>
2021-06-30 11:45:31 -0300

    add thread safe write with barrier

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,19 @@
-import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
+
+class Logger {
+
+  private var hash: [String: String] = [:]
+  private let queue = DispatchQueue(label: "my_queue", attributes: .concurrent)
+
+  func read(_ key: String) -> String? {
+    return self.hash[key]
+  }
+
+  func write(key: String, value: String) {
+    queue.asyncAfter(deadline: DispatchTime.now(), qos: .default, flags: .barrier, execute: {
+      self.hash[key] = value
+    })
+  }
+}
